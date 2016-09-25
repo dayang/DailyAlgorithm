0ab009d71c1d87e461c9df4afdf5fae44e01a3f2
yonghua <1306693844@qq.com>
2016-09-26 01:10:59 +0800

    bubbleSort and quickSort

--- src/DailyAlgorithm.js
+++ src/DailyAlgorithm.js
@@ -5,8 +5,44 @@
  **/
 function DailyAlgorithm(){}
 
+// sort algorithms
+
+//bubbleSort 冒泡排序
+DailyAlgorithm.prototype.bubbleSort = function(array){
+    var i, j, temp, len = array.length;
+    for(i = 0; i < len - 1; i++){
+        for(j = 1; j < len - i; j++){
+            if(array[j] < array[j-1]){
+                temp = array[j-1];
+                array[j-1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+};
+
+//quickSort 快速排序
 DailyAlgorithm.prototype.quickSort = function(array){
-    return "good";
+    sortHelper(0,array.length - 1);
+
+    function sortHelper(left, right) {
+        if(left < right){
+            var low = left, high = right, key = array[low];
+            while(low < high){
+                while(low < high && array[high] >= key){
+                    high --;
+                }
+                array[low] = array[high];
+                while(low < high && array[low] <= key){
+                    low ++;
+                }
+                array[high] = array[low];
+            }
+            array[low] = key;
+            sortHelper(left, low - 1);
+            sortHelper(low + 1, right);
+        }
+    }
 };
 
 module.exports =new DailyAlgorithm();
--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
 var Algorithm = require("../src/DailyAlgorithm");
 var should = require("should");
 
-describe("快速排序",function(){
-    it('return good',function(){
-        should(Algorithm.quickSort()).equal("good");
-    })
+describe("sort algorithms",function(){
+    var array;
+    beforeEach(function(){
+        array = [1,2,3,0,34,56,12,12,34,12,0,-2];
+    });
+
+    it('bubbleSort right',function(){
+        Algorithm.bubbleSort(array);
+        should(array).eql([-2,0,0,1,2,3,12,12,12,34,34,56]);
+    });
+
+    it('quickSort right',function(){
+        Algorithm.quickSort(array);
+        should(array).eql([-2,0,0,1,2,3,12,12,12,34,34,56]);
+    });
 });
